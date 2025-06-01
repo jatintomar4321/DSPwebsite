cb6cfa018dec086fd2594ebe780de405b73919cd
webwizzz <jatin.tomar@somaiya.edu>
2025-06-01 15:17:29 +0530

    updated background

--- src/components/Number.jsx
+++ src/components/Number.jsx
@@ -1,7 +1,7 @@
 
 const Number = () => {
   return (
-    <div className="w-screen min-h-screen flex items-center justify-center bg-[#fff]">
+    <div className="w-screen min-h-screen flex items-center justify-center bg-gradient-to-br from-[#EEF4E9] to-[#D2E6DB]">
       <div className="flex flex-col md:flex-row items-center justify-center gap-[4vw] w-full max-w-[1200px]">
         <div className="flex flex-col items-center justify-center">
           <div className="font-['Playfair_Display'] text-[#18713C] font-bold text-[60px] mb-[0.5vw]">15</div>
